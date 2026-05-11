a6dc802e38d088d0dbc6e04033795f8d93d5d649
LJ Robinson <root@lj-robinson.com>
2026-05-11 15:45:48 -0700

    Add Rust tax calculations

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,6 +2,7 @@ pub mod dosing;
 pub mod potency;
 pub mod pricing;
 pub mod rounding;
+pub mod taxes;
 pub mod weights;
 
 pub use dosing::{dab_mg, total_mg_from_weight_and_potency};
@@ -11,6 +12,8 @@ pub use potency::{total_cbd, total_thc};
 pub use pricing::{price_per_gram, price_per_mg, price_per_mg_from_weight_and_potency};
 
 pub use rounding::{round_to_decimals, round_to_nearest_nickel, rounding_difference};
+
+pub use taxes::{tax_amount, total_with_combined_taxes};
 
 pub use weights::{
     eighths_to_grams, grams_to_ounces, grams_to_pounds, grams_to_retail_eighths, ounces_to_grams,
