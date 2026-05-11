03df82d549f2f69543685576b8be9a96fba23792
LJ Robinson <root@lj-robinson.com>
2026-05-11 15:20:55 -0700

    Organize Rust potency module

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,24 +1,3 @@
-pub fn total_thc(thc: f64, thca: f64) -> f64 {
-    thc + thca * 0.877
-}
+pub mod potency;
 
-pub fn total_cbd(cbd: f64, cbda: f64) -> f64 {
-    cbd + cbda * 0.877
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn calculates_total_thc() {
-        let result = total_thc(1.0, 20.0);
-        assert!((result - 18.54).abs() < 0.0001);
-    }
-
-    #[test]
-    fn calculates_total_cbd() {
-        let result = total_cbd(0.5, 10.0);
-        assert!((result - 9.27).abs() < 0.0001);
-    }
-}
+pub use potency::{total_cbd, total_thc};
